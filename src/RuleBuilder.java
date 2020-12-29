fa37781e91ac3eb17e76e0cc5ed79f532be0b27f
Bownayr <70776863+Bownayr@users.noreply.github.com>
2020-12-28 19:47:58 -0500

    Update RuleBuilder.java

--- src/RuleBuilder.java
+++ src/RuleBuilder.java
@@ -1,8 +1,11 @@
-
+import java.util.ArrayList;
 public class RuleBuilder {
 	
 	final String set; // required
 	final int wincondition; //require 1 = normal, 2 = accuracy, 3 = scoreV2
+	final ArrayList<Map> mappool;
+	final int bestoutof;
+	final ArrayList<String> players; //The players in this tourney.
 	boolean bans; // opitonal 
 	int numbans; // optional
 	boolean mods; //optional
@@ -11,10 +14,16 @@ public class RuleBuilder {
 	 * Constructor for rule builer
 	 * @param set Name of the rule set
 	 * @param wincon Win condition for tourney
+	 * @param pool the arraylist containing all the maps
+	 * @param best what the tourney is out of
+	 * @param players an arraylist containing the names of all participating players
 	 */
-	public RuleBuilder(String set, int wincon) {
+	public RuleBuilder(String set, int wincon, ArrayList<OsuMap> pool, int best, ArrayList<String> players) {
 		this.set = set;
 		this.wincondition = wincon;
+		this.mappool = pool;
+		this.bestoutof = best;
+		this.players = players;
 	}
 	/**
 	 * Setter method for changing bans
@@ -46,7 +55,39 @@ public class RuleBuilder {
 		this.mods = bool;
 		return this;
 	}
+	/**
+	 * Setter methods for Map pool
+	 * @param pool
+	 * @return
+	 */
+	public RuleBuilder mappool(ArrayList<OsuMap> pool) {
+		this.mappool = pool;
+		return this;
+	}
 	
+	/**
+	 * Setter method for best out of
+	 * @param best
+	 * @return
+	 */
+	public RuleBuilder bestoutof(int best) {
+		this.bestoutof = best;
+		return this;
+	}
+	
+	/**
+	 * Setter method for the player array
+	 * @param player array of player names
+	 * @return
+	 */
+	public RuleBuilder players(ArrayList<String> player) {
+		this.players = player;
+		return this;
+	}
+	/**
+	 * Builder method that creates rule set.
+	 * @return
+	 */
 	public Rule build() {
 		Rule set = new Rule(this);
 		return set;
