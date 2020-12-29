07e094139efbebf1bb21536016e1c2ea9b95ac84
Bownayr <70776863+Bownayr@users.noreply.github.com>
2020-12-28 23:23:56 -0500

    Update Rule.java

--- src/tournamentData/Rule.java
+++ src/tournamentData/Rule.java
@@ -4,16 +4,19 @@ import java.util.Map;
 
 public class Rule {
 	
-	final String set; // required
-	final int wincondition; //require 1 = normal, 2 = accuracy, 3 = scoreV2
+	final String RuleSetName; // required
+	
 	final MapPool mappool; // required mappool
-	final int bestoutof; // how many games to be played
+	final int firstto; // how many games to be played
 	final ArrayList<String> players; //The players in this tourney.
 	final int teamsize; // The size of a team
-	int readytime, picktime, bantime; // for the amount of time given
-	//to ready up, pick map and ban maps
+	
+	int wincondition; //require 1 = normal, 2 = accuracy, 3 = scoreV2
+	int readytime, picktime, bantime; // for the amount of time given to ready up, pick map and ban maps
 	int numbans; // optional
 	boolean warmup; // optional
+	int maxWarmupLength; //optional
+	
 	/**
 	 * The actual ruleset that takes in a builder
 	 * @param builder The builder for this rule set.
@@ -25,19 +28,24 @@ public class Rule {
 		this.picktime = builder.picktime;
 		this.bantime = builder.bantime;
 		this.teamsize = builder.teamsize;
-		this.set = builder.set;
+		this.RuleSetName = builder.set;
 		this.wincondition = builder.wincondition;
 		this.numbans = builder.numbans;
 		this.mappool = builder.mappool;
 		this.players = builder.players;
-		
+		this.firstto = builder.firstto;
+		this.maxWarmupLength = builder.maxWarmupLength;
 	}
 	private int getwincon() {
 		return this.wincondition;
 	}
 	
-	private String getSet() {
-		return set;
+	private int getmaxWarmupLength() {
+		return maxWarmupLength;
+	}
+	
+	private String getRuleSetName() {
+		return RuleSetName;
 	}
 	private int getWincondition() {
 		return wincondition;
@@ -45,8 +53,8 @@ public class Rule {
 	private MapPool getMappool() {
 		return mappool;
 	}
-	private int getBestoutof() {
-		return bestoutof;
+	private int getfirstto() {
+		return firstto;
 	}
 	private ArrayList<String> getPlayers() {
 		return players;
@@ -71,19 +79,6 @@ public class Rule {
 	}
 	private int getbans() {
 		return this.numbans;
-	}
-	
-	private
-	
-	/**
-	 * Method for banning an osu map.
-	 * Removes map from pool if banned.
-	 * @return Returns true if map has been banned false if not.
-	 */
-	private boolean ban(OsuMap) {
-		return mappool.remove(OsuMap);
-			
-		}
 	}
 	
 	
