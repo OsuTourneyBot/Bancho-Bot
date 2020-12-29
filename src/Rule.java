baac4b8e22570d1c72af3eeb5edb092957fc028e
Bownayr <70776863+Bownayr@users.noreply.github.com>
2020-12-28 19:47:55 -0500

    Update Rule.java

--- src/Rule.java
+++ src/Rule.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Rule {
 	
 	final String set; // required
 	final int wincondition; //require 1 = normal, 2 = accuracy, 3 = scoreV2
+	final ArrayList<OsuMap> mappool; // required mappool
+	final int bestoutof; // how many games to be played
+	final ArrayList<String> players; //The players in this tourney.
 	boolean bans; // opitonal 
 	int numbans; // optional
 	boolean mods; //optional
@@ -18,6 +23,8 @@ public class Rule {
 		this.bans = builder.bans;
 		this.numbans = builder.numbans;
 		this.mods = builder.mods;
+		this.mappool = builder.mappool;
+		this.players = builder.players;
 		
 	}
 	public int getwincon() {
@@ -30,6 +37,17 @@ public class Rule {
 	
 	public boolean getmods() {
 		return this.mods;
+	}
+	
+	/**
+	 * Method for banning an osu map.
+	 * Removes map from pool if banned.
+	 * @return Returns true if map has been banned false if not.
+	 */
+	public boolean ban(OsuMap) {
+		return mappool.remove(OsuMap);
+			
+		}
 	}
 	
 	
