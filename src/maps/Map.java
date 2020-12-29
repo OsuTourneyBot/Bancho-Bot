5dca2c5c13ff01556e8484f7924bfb3d9b3434c6
vincent <54919042+vince212121@users.noreply.github.com>
2020-12-28 21:20:06 -0500

    added map pool stuff

---
+++ src/maps/Map.java
@@ -0,0 +1,38 @@
+package maps;
+
+public class Map 
+{
+	// stores the song ID, song link, and mod
+	private int ID;
+	private String link;
+	private int mod;
+	private boolean freeMod;
+	
+	Map(int ID, String link, int mod, boolean freeMod)
+	{
+		this.ID = ID;
+		this.link = link;
+		this.mod = mod;
+		this.freeMod = freeMod;
+		
+	}
+
+	// getters
+	public String getLink() {
+		return link;
+	}
+
+	public int getMod() {
+		return mod;
+	}
+
+	public boolean isFreeMod() {
+		return freeMod;
+	}
+
+	public int getID() {
+		return ID;
+	}
+	
+	
+}
---
+++ src/maps/MapPool.java
@@ -0,0 +1,71 @@
+/**
+ * Need to make maps that uses the beatmap ID as the key
+ * Need 2 maps, one that holds mods and the other one that holds the song/link to download
+ * 
+ * How to use hashmaps in java
+ * https://www.w3schools.com/java/java_hashmap.asp 
+ */
+package maps;
+
+import java.util.HashMap;
+
+/**
+ * @author Vince
+ *
+ */
+public class MapPool 
+{
+	// stores songs and mods
+	private HashMap<Integer, Map> songPool;
+	private HashMap<String, Map> tournamentName;
+	
+	// constructor
+	MapPool()
+	{
+		this.songPool = new HashMap<Integer, Map>();
+		this.tournamentName = new HashMap<String, Map>();
+	}
+
+	/**
+	 * find if the map has the ID
+	 * @param ID
+	 * @return
+	 */
+	public boolean hasMap (Integer ID)
+	{
+		return this.songPool.containsKey(ID);
+	}
+	
+	/**
+	 * find if the map has the ID
+	 * @param ID
+	 * @return
+	 */
+	public boolean hasTournament (String ID)
+	{
+		return this.tournamentName.containsKey(ID);
+	}
+	
+	/**
+	 * Inserts map into pool
+	 * @param name
+	 * @param map
+	 */
+	public void insertMap(String name, Map map)
+	{
+		// insert into mod and song pool
+		// the key itself is the songID which is shared between the mods and song pool
+		this.tournamentName.put(name, map);
+		this.songPool.put(map.getID(), map);
+	}
+	
+	public Map getSong(Integer ID)
+	{
+		return this.songPool.get(ID);
+	}
+	
+	public Map getTournament(String ID)
+	{
+		return this.tournamentName.get(ID);
+	}
+}
