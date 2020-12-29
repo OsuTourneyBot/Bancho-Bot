d4f052ec7afdc85c6cc15ab3ab2ecc7d9c133346
Bownayr <70776863+Bownayr@users.noreply.github.com>
2020-12-28 23:23:58 -0500

    Update RuleBuilder.java

--- src/tournamentData/RuleBuilder.java
+++ src/tournamentData/RuleBuilder.java
@@ -3,15 +3,19 @@ import java.util.ArrayList;
 public class RuleBuilder {
 	
 	final String set; // required
-	final int wincondition; //require 1 = normal, 2 = accuracy, 3 = scoreV2
+	
 	final MapPool mappool;
-	final int bestoutof;
+	final int firstto;
 	final ArrayList<String> players; //The players in this tourney.
-	final int teamsize; // Number of players in a team 
-	int readytime, picktime, bantime; // for the amount of time given
-	//to ready up, pick map and ban maps
-	int numbans; // optional
-	boolean warmup;
+	final int teamsize; // Number of players in a team.
+	final int numbans; // number of bans per team.
+	
+	int wincondition = 3; //require 1 = normal, 2 = accuracy, 3 = scoreV2, ScoreV2 by default
+	int readytime = 120; // The time given to warmup, default at 120 seconds.
+	int picktime = 120; // The time given to pick maps, default at 120 seconds.
+	int bantime = 120; // The time given to ban, default at 120 seconds.
+	boolean warmup = true; //Check if warmup is enabled true by default.
+	int maxWarmupLength = 300; //The max length of a warmup map, defaults at 300 seconds.
 	/**
 	 * Constructor for rule builer
 	 * @param set Name of the rule set
@@ -20,32 +24,31 @@ public class RuleBuilder {
 	 * @param best what the tourney is out of
 	 * @param players an arraylist containing the names of all participating players
 	 */
-	public RuleBuilder(String set, int wincon, MapPool pool, int best, ArrayList<String> players, int team) {
+	public RuleBuilder(String set, MapPool pool, int best, ArrayList<String> players, int team, int numbans) {
 		this.set = set;
 		this.wincondition = wincon;
 		this.mappool = pool;
-		this.bestoutof = best;
+		this.firstto = best;
 		this.players = players;
 		this.teamsize = team;
+		this.numbans = numbans;
 	}
-
 	/**
-	 * Setter method for number of bans
-	 * 
-	 * @param number of bands
-	 * @return This object
+	 * Setter method for type of win condition.
+	 * 1 = normal, 2 = accuracy , 3 = ScoreV2
+	 * @param cond
+	 * @return
 	 */
-	protected RuleBuilder numbans (int num) {
-		this.numbans = num;
+	protected RuleBuilder wincondition(int cond) {
+		this.wincondition = cond;
 		return this;
 	}
-
 	/**
 	 * Setter methods for Map pool
 	 * @param pool
 	 * @return
 	 */
-	protected RuleBuilder mappool(ArrayList<OsuMap> pool) {
+	protected RuleBuilder mappool(MapPool pool) {
 		this.mappool = pool;
 		return this;
 	}
@@ -55,8 +58,8 @@ public class RuleBuilder {
 	 * @param best
 	 * @return
 	 */
-	protected RuleBuilder bestoutof(int best) {
-		this.bestoutof = best;
+	protected RuleBuilder firstto(int best) {
+		this.firstto= best;
 		return this;
 	}
 	
@@ -106,6 +109,17 @@ public class RuleBuilder {
 		this.warmup = bool;;
 		return this;
 	}
+	
+	/**
+	 * Setter for warm up length
+	 * @param time time in seconds.
+	 * @return
+	 */
+	protected RuleBuilder maxWarmupLength(int time) {
+		this.maxWarmupLength = time;
+		return this;
+	}
+	
 	/**
 	 * Builder method that creates rule set.
 	 * @return
@@ -114,5 +128,7 @@ public class RuleBuilder {
 		Rule set = new Rule(this);
 		return set;
 	}
+	
+	
 	
 } 
