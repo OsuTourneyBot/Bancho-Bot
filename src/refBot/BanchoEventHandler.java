383255dfc52a628f6c60b6e24a0d6ead368308e8
Bownayr <70776863+Bownayr@users.noreply.github.com>
2020-12-29 22:54:39 -0500

    Update BanchoEventHandler.java
    Added cases for when players ready up and finish playing

--- src/refBot/BanchoEventHandler.java
+++ src/refBot/BanchoEventHandler.java
@@ -2,17 +2,29 @@ package refBot;
 
 import irc.IRCClient;
 import irc.handlers.IRCEventHandler;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BanchoEventHandler implements IRCEventHandler {
 	private RefBot ref;
-
+	private Pattern maplink = Pattern.compile("Changed beatmap to https://osu.ppy.sh/b/[0-9]* .*");
+	
 	BanchoEventHandler(RefBot ref) {
 		this.ref = ref;
 	}
-
+	
 	@Override
 	public String[] match(String[] data) {
-		if (data[0].toLowerCase().startsWith("countdown finished")) {
+		
+		
+		if (data[0].toLowerCase().contains("finished playing")){
+			return data;
+		}
+		else if (data[0].toLowerCase().contains("all players are ready")) {
+			return data;
+		}
+		
+		else if (data[0].toLowerCase().startsWith("countdown finished")) {
 			return data;
 		} else {
 			return null;
