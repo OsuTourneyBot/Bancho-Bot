d6968066d3847723c86410c3ba92db997dd2b310
Bownayr <70776863+Bownayr@users.noreply.github.com>
2021-01-02 01:00:43 -0500

    Update APIMapInfo.java

--- src/api/APIMapInfo.java
+++ src/api/APIMapInfo.java
@@ -9,49 +9,65 @@ import java.net.URL;
 import org.json.JSONObject;
 
 public class APIMapInfo {
-	String ID;
-	int Length;
-	String Mode;
-	private String AccessString;
-	private String[] Parent;
-	
+	private String accessString;
+	private String[] parent;
 	URL beatmapLink;
+
 	/**
 	 * The constructor for the Get Map object.
-	 * @param link Takes in the link of the beat map
+	 * 
+	 * @param link  Takes in the link of the beat map
 	 * @param token The JSON object for the token.
 	 * @throws IOException
 	 */
 	public APIMapInfo(String link, APIToken token) throws IOException {
-	// Get API token
-	
-	if  (token.isExpired()){
-		token.GenerateCode();
-		
-	}
-	else {
-		String AccessString = token.pullData();
-		String Bearer = "Bearer " + AccessString;
-		String body = API.GET(link, "Authorization","Accept", Bearer , "application/json");
-		Parent = body.split(",");
-	}	
-	
-	}
-	
-	public String GetTitle() {
-		return Parent[43].split(":")[1];
-	}
-	
-	public Object GetID() {
-		return Parent[1].split(":")[1];
-	}
-	
-	public String GetLength() {
-		return Parent[3].split(":")[1];
-	}
-	public String GetMode() {
-		return Parent[2].split(":")[1];
-	}
-	
-}
 
+		if (token.isExpired()) {
+			token.GenerateCode();
+		} else {
+			String AccessString = token.pullData();
+			String Bearer = "Bearer " + AccessString;
+			String[] Header = new String[] { "Authorization", "Accept" };
+			String[] Body = new String[] { Bearer, "application/json" };
+			String body = API.GET(link, Header, Body);
+			parent = body.split(",");
+		}
+	}
+
+	/**
+	 * Returns the title of the beatmap
+	 * 
+	 * @return
+	 */
+	public String GetTitle() {
+		return parent[43].split(":")[1];
+	}
+
+	/**
+	 * returns the Map ID
+	 * 
+	 * @return
+	 */
+	public Object GetID() {
+		return parent[1].split(":")[1];
+	}
+
+	/**
+	 * Returns the length in seconds
+	 * 
+	 * @return
+	 */
+	public String GetLength() {
+		return parent[3].split(":")[1];
+	}
+
+	/**
+	 * Returns the mode of the map
+	 * 
+	 * @return
+	 */
+	public String GetMode() {
+		return parent[2].split(":")[1];
+	}
+
+}
