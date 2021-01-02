4781bafe81849c5f5a3008fed93d9dfd8ec3d7bc
Fungucide Fug Not Again <wc.fung@mail.utoronto.ca>
2021-01-02 03:31:52 -0500

    Updated OSU API

--- src/api/APIToken.java
+++ src/api/APIToken.java
@@ -6,10 +6,10 @@ import org.json.JSONObject;
 public class APIToken {
 
 	private static JSONObject parent = new JSONObject();
-	JSONObject obj = new JSONObject();
+	private static JSONObject obj = new JSONObject();
 	private static String tokenCodes;
-	long initiatedTime;
-	int expiraryTime;
+	private static long initiatedTime;
+	private static int expiraryTime;
 
 	/**
 	 * The constructor for trying to get an API Token.
@@ -33,7 +33,7 @@ public class APIToken {
 	 * 
 	 * @return JSON Object containing the token.
 	 */
-	public String pullData() {
+	private static String pullData() {
 		return tokenCodes;
 	}
 
@@ -42,7 +42,7 @@ public class APIToken {
 	 * 
 	 * @return True if Expired, False if not expired.
 	 */
-	public boolean isExpired() {
+	private static boolean isExpired() {
 		return ((initiatedTime + (1000 * expiraryTime) <= System.currentTimeMillis()));
 	}
 
@@ -52,7 +52,7 @@ public class APIToken {
 	 * 
 	 * @throws IOException
 	 */
-	public void GenerateCode() throws IOException {
+	private static void GenerateCode() throws IOException {
 		String[] Header = new String[] { "Accept", "Content-Type" };
 		String[] Body = new String[] { "application/json", "application/json ;charset=UTF-8" };
 		String body = API.POST("https://osu.ppy.sh/oauth/token", obj.toString(), Header, Body);
@@ -62,6 +62,19 @@ public class APIToken {
 		tokenCodes = number[2].split(":")[1].substring(1, number[2].split(":")[1].length() - 1);
 		initiatedTime = System.currentTimeMillis();
 
+	}
+
+	public static JSONObject APIMapInfo(int id) throws IOException {
+		if (isExpired()) {
+			GenerateCode();
+		}
+		String AccessString = pullData();
+		String bearer = "Bearer " + AccessString;
+		String[] header = new String[] { "Authorization", "Accept" };
+		String[] body = new String[] { bearer, "application/json" };
+		String stringBody = API.GET("https://osu.ppy.sh/api/v2/beatmaps/" + id, header, body);
+		JSONObject data = new JSONObject(stringBody);
+		return data;
 	}
 
 }
